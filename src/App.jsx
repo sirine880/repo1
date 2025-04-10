d9e7f8a5d5c3c04984f40a8642e2d142ef9c5d4f
Sirine Sboui <sirinesboui95@example.com>
2025-04-10 12:35:19 +0200

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,69 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function App() {
+function getTitle(title) {
+  return title;
+}
+
+
+const list = [
+  {
+    title: "React",
+    url: "http://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "http://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
+function Search() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
-      <label htmlFor="search">search: </label>
-      <input id="search" type="text"></input>
-      <hr />
-      <ul>{list.map(function(item) {
-        return <li key={item.objectID}><span><a href={item.url}>{item.title}</a></span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span></li>;
-      })}
-      </ul>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
+
+function List() {
+  return (
+    <ul>
+      {list.map(function(item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> {item.author} </span>
+            <span> {item.num_comments} </span>
+            <span> {item.points} </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
+    </div>
+  );
+}
+
 export default App;
-const list = [{
-  title:"React" ,
-  url : "https://reactifsorg/" ,
-  author: "jordan walke" ,
-  num_comments : 2,
-  points: 5,
-  objectID: 1,
-},
-];
-    
